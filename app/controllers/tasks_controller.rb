289947e688b19d5db152e0903eaf43b52c1814b3
Aralle <nsugita@salesforce.com>
2021-08-05 16:56:22 +0000

    add login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,14 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:destroy]
     
     def index
         @tasks = Task.all
+        if logged_in?
+            @tasks = current_user.tasks.build  # form_with 用
+            @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+        end
     end
     
     def show
@@ -14,13 +20,14 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
     
         if @task.save
             flash[:success] = 'タスクが正常に登録されました'
-            redirect_to @task
+            redirect_to root_url
         else
-            flash.now[:danger] = 'タスクが登録されませんでした'
+            @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+            flash.now[:danger] = 'タスクが登録に失敗しました'
             render :new
         end
     end
@@ -33,7 +40,7 @@ class TasksController < ApplicationController
             flash[:success] = 'タスクは正常に更新されました'
             redirect_to @task
         else
-            flash.now[:danger] = 'タスクは更新されませんでした'
+            flash.now[:danger] = 'タスクの更新に失敗しました'
             render :edit
         end        
     end
@@ -44,14 +51,21 @@ class TasksController < ApplicationController
         flash[:success] = 'タスクは正常に削除されました'
         redirect_to tasks_url
     end
-end
+
+    private
     
-private
-
-def set_task
-    @task = Task.find(params[:id])
-end
-
-def task_params
-    params.require(:task).permit(:content, :status)
+    def set_task
+        @task = Task.find(params[:id])
+    end
+    
+    def task_params
+        params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+    end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+    #UserとTask の一対多
+    belongs_to :user
+    
     #バリデーション
     validates :content, presence: true, length: { maximum: 255 }
     validates :status, presence: true, length: { maximum: 10 }
