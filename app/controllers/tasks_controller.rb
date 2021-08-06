b1ed8704c2d2f9caa8ab0b8cd8663ae965209ebd
Aralle <nsugita@salesforce.com>
2021-08-06 10:51:53 +0000

    add login2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,8 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
-    before_action :correct_user, only: [:destroy]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
-        @tasks = Task.all
         if logged_in?
             @tasks = current_user.tasks.build  # form_with 用
             @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
@@ -53,10 +51,6 @@ class TasksController < ApplicationController
     end
 
     private
-    
-    def set_task
-        @task = Task.find(params[:id])
-    end
     
     def task_params
         params.require(:task).permit(:content, :status)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'toppages/index'
+  # get 'toppages/index'
   get 'users/index'
   get 'users/show'
   get 'users/new'
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     
     root to: 'tasks#index'
-
-     #root to: 'toppages#index'
     
     resources :tasks
     
